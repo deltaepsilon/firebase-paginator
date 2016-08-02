993bc39a7462411c8203254d1b7f7577f48d4bcd
Chris Esplin <chris@quiver.is>
2016-08-02 10:30:55 -0600

    seed gh-pages

---
+++ components/firebase-paginator/README.md
@@ -0,0 +1,224 @@
+# FirebasePaginator
+
+FirebasePaginator is a JavaScript utility for Node.js and the browser that enables simple, declarative pagination for your Firebase collections. It's been developed for Firebase 3.0, but it should work for Firebase 2.0 projects as well.
+
+### Dependencies
+
+FirebasePaginator relies on the [Promise](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise) object. Promise support is great for modern versions of Chrome, Firefox, Edge, Opera and Safari. Internet Explorer gets left out. Sorry IE, but even Microsoft is sick of you. You're dying. And attempting to support you would only prolong the misery.
+
+FirebasePaginator uses the [Axios](https://github.com/mzabriskie/axios) library for Node.js and XMLHttpRequest in the browser.
+
+In summary: Node.js requires Promise and Axios; the browser requires Promise and XMLHttpRequest.
+
+### Install
+
+- NPM: ```npm install --save firebase-paginator```
+- Bower: ```bower install --save firebase-paginator```
+
+### Test
+
+- ```npm install```
+- ```npm test```
+
+### Usage
+
+If you're in Node.js, you'll need to do something like ```var FirebasePaginator = require('firebase-paginator')```. 
+
+If you're in the browser, you'll have access to FirebasePaginator on the ```window``` object like so: ```var FirebasePaginator = window.FirebasePaginator;```
+
+Once you have your ```FirebasePaginator``` object, the rest is isomorphic JavaScript. Just pass in a Firebase ref and some options:
+
+***pageSize***: any integer greater than zero, defaults to 10
+
+***finite***: defaults to false
+
+***auth***: optional auth token for secure collections
+
+```
+var options = {
+  pageSize: 15,
+  finite: true,
+  auth: 'MyAuthTokenForSecurityPurposes'
+};
+var paginator = new FirebasePaginator(ref);
+
+```
+# Functions
+
+#### FirebasePaginator.prototype.listen(callback)
+
+Listens to all events
+
+Useful for proxying events or just debugging
+
+```
+var paginator = new FirebasePaginator(ref);
+var itemsList = [];
+
+paginator.listen(function (eventName, eventPayload) {
+  console(`Fired ${eventName} with the following payload: `, eventPayload);
+});
+``` 
+
+
+#### FirebasePaginator.prototype.on(event, callback)
+
+Attaches a callback to an event
+
+```
+var paginator = new FirebasePaginator(ref);
+var itemsList = [];
+var handler = function() {
+  collection = paginator.collection();
+};
+
+paginator.on('value', handler);
+``` 
+
+#### FirebasePaginator.prototype.off(event, callback)
+
+Detaches a callback from an event
+
+```
+var paginator = new FirebasePaginator(ref);
+var itemsList = [];
+var handler = function() {
+  collection = paginator.collection();
+};
+
+paginator.off('value', handler);
+``` 
+
+#### FirebasePaginator.prototype.once(event, callback) -> returns promise
+
+Calls a callback exactly once for an event
+
+```
+var paginator = new FirebasePaginator(ref);
+var itemsList = [];
+var handler = function() {
+  collection = paginator.collection();
+};
+
+// Callback pattern
+paginator.once('value', handler);
+
+// Promise pattern
+paginator.once('value').then(handler);
+``` 
+
+#### FirebasePaginator.prototype.reset() -> returns promise
+
+Resets pagination
+
+Infinite: jumps to end of collection
+
+Finite: Refreshes keys list and jumps to page 1         
+
+```
+var paginator = new FirebasePaginator(ref);
+paginator.reset()
+  .then(function() {
+    console.log('list has been reset');
+  });
+``` 
+
+#### FirebasePaginator.prototype.previous() -> returns promise
+
+Pages backward
+
+```
+var paginator = new FirebasePaginator(ref);
+paginator.previous()
+  .then(function() {
+    console.log('paginated backward');
+  });
+``` 
+
+
+#### FirebasePaginator.prototype.next() -> returns promise
+
+Pages forward
+
+```
+var paginator = new FirebasePaginator(ref);
+paginator.next()
+  .then(function() {
+    console.log('paginated forward');
+  });
+``` 
+
+#### FirebasePaginator.prototype.goToPage(<page number>) -> returns promise
+
+Jumps to any page
+
+Accepts page numbers from 1 to the pageCount
+
+Available for finite pagination ***only***
+
+```
+var paginator = new FirebasePaginator(ref);
+paginator.goToPage(3)
+  .then(function() {
+    console.log('paginated to page 3');
+  });
+``` 
+
+# Events
+
+#### value
+
+The **value** event fires after every change in data. FirebasePaginator listens to the Firebase **value** event, manipulates the data a bit and then fires its own **value** event.
+
+#### isLastPage
+
+**isLastPage** fires just after the **value** event if FirebasePaginator has reached the top of the list.
+
+#### ready
+
+FirebasePaginator fires its **ready** event once the first page is loaded.
+
+#### reset, next, previous
+
+The **reset**, **next** and **previous** events fire after each of the corresponding functions is complete and the new data is loaded.
+
+
+# Finite vs Infinite Pagination
+
+There are two ways to paginate Firebase data: finite and infinite paginations.
+
+Let's assume that pageSize is 10 and we have records 1 through 100. Also note that all Firebase pagination occurs from the bottom of the collection. 
+
+#### Infinite Pagination
+
+Infinite pagination pulls the last 11 records of the collection, saves the 90th record's key as a cursor and adds records 91 through 100 to the collection.
+
+Infinite pagination steps backward by pulling another 11 records ending at the cursor (a.k.a. the 90th record's key). So paging back once will display records 81 to 90 with record 80's key as the new cursor. Page back again and you're at records 71 to 80 and so forth.
+
+Pros:
+
+- Scales forever
+- Users can page forward to discover new records as they're added to the collection
+- If a user is on the first page, new records will simply appear as they are added
+
+Cons:
+
+- Must page forward and backward sequentially. Can't skip pages.
+- No context for how many pages exist and where the user is in the list
+- If a user is on the first page, new records will simply appear as they are added
+
+#### Finite Pagination
+
+Finite pagination makes a single "shallow" REST query to pull all of the collection's keys. See [the docs](https://firebase.google.com/docs/reference/rest/database/#section-param-shallow) on how this is done.  
+
+Once FirebasePaginator has all of the keys, it sorts them and finds the page endpoints. So if we have 100 records with a pageSize of 10, the page endpoints will be the keys for records 10, 20, 30, 40, 50... 100.
+
+Pros:
+
+- Users have context for where they are in the collection.
+- Users can skip pages.
+
+Cons:
+
+- Beware of scaling issues. Consider archiving records to [Google Cloud Datastore](https://cloud.google.com/datastore/docs/) if the collection grows too large.
+- Must call ```paginator.reset()``` to capture new records the may be added
---
+++ components/firebase-paginator/firebase-paginator.js
@@ -0,0 +1,292 @@
+var isWindow = typeof window === 'object';
+var FirebasePaginator = function (ref, defaults) {
+  var paginator = this;
+  var defaults = defaults || {};
+  var pageSize = defaults.pageSize ? parseInt(defaults.pageSize, 10) : 10;
+  var isFinite = defaults.finite ? defaults.finite : false;
+  var auth = defaults.auth;
+
+  // Events
+  this.listen = function (callback) {
+    paginator.allEventHandler = callback;
+  };
+  var events = {};
+  var fire = function (eventName, payload) {
+    if (typeof paginator.allEventHandler === 'function') {
+      paginator.allEventHandler.call(paginator, eventName, payload);
+    }
+
+    if (events[eventName] && events[eventName].queue) {
+      var queue = events[eventName].queue.reverse();
+      var i = queue.length;
+      while (i--) {
+        if (typeof queue[i] === 'function') {
+          queue[i].call(paginator, payload);
+        }
+      }
+    }
+  };
+
+  this.on = function (eventName, callback) {
+    if (!events[eventName]) {
+      events[eventName] = {
+        queue: []
+      };
+    }
+    events[eventName].queue.push(callback);
+  };
+
+  this.off = function (eventName, callback) {
+    if (events[eventName] && events[eventName].queue) {
+      var queue = events[eventName].queue;
+      var i = queue.length;
+      while (i--) {
+        if (queue[i] === callback) {
+          queue.splice(i, 1);
+        }
+      }
+    }
+  };
+
+  this.once = function (eventName, callback) {
+    return new Promise(function (resolve, reject) {
+      var handler = function (payload) {
+        paginator.off(eventName, handler);
+        if (typeof callback === 'function') {
+          try {
+            resolve(callback.call(paginator, payload));
+          } catch (e) {
+            reject(e);
+          }
+        } else {
+          resolve(payload);
+        }
+      };
+      paginator.on(eventName, handler);
+    });
+
+  };
+
+  /*
+   *  Pagination can be finite or infinite. Infinite pagination is the default.
+   */
+  if (!isFinite) { // infinite pagination
+
+    var setPage = function (cursor, isForward, isLastPage) {
+      this.ref = ref.orderByKey();
+
+      // If there it's forward pagination, use limitToFirst(pageSize + 1) and startAt(theLastKey)
+
+      if (isForward) { // forward pagination
+        this.ref = this.ref.limitToFirst(pageSize + 1);
+        if (cursor) { // check for forward cursor
+          this.ref = this.ref.startAt(cursor);
+        }
+      } else { // previous pagination
+        this.ref = this.ref.limitToLast(pageSize + 1);
+        if (cursor) { // check for previous cursor
+          this.ref = this.ref.endAt(cursor);
+        }
+      }
+
+      return this.ref.once('value')
+        .then(function (snap) {
+          var numChildren = snap.numChildren();
+          var keys = [];
+          var collection = {};
+
+          cursor = undefined;
+
+          snap.forEach(function (childSnap) {
+            keys.push(childSnap.key);
+            if (!cursor) {
+              cursor = childSnap.key;
+            }
+            collection[childSnap.key] = childSnap.val();
+          });
+
+
+          if (keys.length === pageSize + 1) {
+            if (isLastPage) {
+              delete collection[keys[keys.length - 1]];
+            } else {
+              delete collection[keys[0]];
+            }
+          } else if (isForward) {
+            return setPage(); // force a reset if forward pagination overruns the last result
+          } else {
+            return setPage(undefined, true, true);
+          }
+
+          this.snap = snap;
+          this.keys = keys;
+          this.isLastPage = isLastPage || false;
+          this.collection = collection;
+          this.cursor = cursor;
+
+          fire('value', snap);
+          if (this.isLastPage) {
+            fire('isLastPage');
+          }
+          return this;
+        }.bind(this));
+    }.bind(this);
+
+    setPage()
+      .then(function () {
+        fire('ready', paginator);
+      }); // bootstrap the list
+
+    this.reset = function () {
+      return setPage()
+        .then(function () {
+          return fire('reset');
+        });
+    };
+
+    this.previous = function () {
+      return setPage(this.cursor)
+        .then(function () {
+          return fire('previous');
+        }.bind(this));
+    };
+
+    this.next = function () {
+      var cursor;
+      if (this.keys && this.keys.length) {
+        cursor = this.keys[this.keys.length - 1];
+      }
+      return setPage(cursor, true)
+        .then(function () {
+          return fire('next');
+        });
+    };
+
+
+  } else { // finite pagination
+    var queryPath = ref.toString() + '.json?shallow=true';
+    if (auth) {
+      queryPath += '&auth=' + auth;
+    }
+    var getKeys = function () {
+      if (isWindow) {
+        return new Promise(function (resolve, reject) {
+          var request = new XMLHttpRequest();
+          request.onreadystatechange = function () {
+            if (request.readyState === 4) {
+              var response = JSON.parse(request.responseText);
+              if (request.status === 200) {
+                resolve(Object.keys(response));
+              } else {
+                reject(response);
+              }
+            }
+          };
+          request.open('GET', queryPath, true);
+          request.send();
+        });
+      } else {
+        var axios = require('axios');
+        return axios.get(queryPath)
+          .then(function (res) {
+            return Object.keys(res.data);
+          });
+      }
+    };
+
+    this.goToPage = function (pageNumber) {
+      pageNumber = Math.min(this.pageCount, Math.max(1, parseInt(pageNumber)));
+      paginator.page = this.pages[pageNumber];
+      paginator.pageNumber = pageNumber;
+      paginator.isLastPage = pageNumber === Object.keys(paginator.pages).length;
+      paginator.ref = ref.orderByKey().limitToLast(pageSize).endAt(paginator.page.endKey);
+
+      return this.ref.once('value')
+        .then(function (snap) {
+          var collection = snap.val();
+          var keys = [];
+
+          snap.forEach(function (childSnap) {
+            keys.push(childSnap.key);
+          });
+
+          paginator.snap = snap;
+          paginator.keys = keys;
+          paginator.collection = collection;
+
+          fire('value', snap);
+          if (paginator.isLastPage) {
+            fire('isLastPage');
+          }
+          return paginator;
+        });
+    };
+
+    this.reset = function () {
+      return getKeys()
+        .then(function (keys) {
+          var orderedKeys = keys.sort();
+          var keysLength = orderedKeys.length;
+          var cursors = [];
+
+          for (var i = keysLength; i > 0; i -= pageSize) {
+            cursors.push({
+              fromStart: {
+                startRecord: i - pageSize + 1,
+                endRecord: i
+              },
+              fromEnd: {
+                startRecord: keysLength - i + 1,
+                endRecord: keysLength - i + pageSize
+              },
+              endKey: keys[i - 1]
+            });
+          }
+
+          var cursorsLength = cursors.length
+          var k = cursorsLength;
+          var pages = {};
+          while (k--) {
+            cursors[k].pageNumber = k + 1;
+            pages[k + 1] = cursors[k];
+          }
+          paginator.pageCount = cursorsLength;
+          paginator.pages = pages;
+
+          return pages;
+        })
+        .catch(function (err) {
+          console.log('finite reset pagination error', err);
+        });
+    };
+
+    this.reset() // Refresh keys and go to first page.
+      .then(function () {
+        return paginator.goToPage(1);
+      })
+      .then(function () {
+        fire('ready', paginator);
+      });
+
+    this.previous = function () {
+      return this.goToPage(Math.min(this.pageCount, this.pageNumber + 1))
+        .then(function() {
+          return fire('previous');
+        });
+    };
+
+    this.next = function () {
+      return this.goToPage(Math.max(1, this.pageNumber - 1))
+        .then(function () {
+          return fire('next');
+        });
+    };
+
+  }
+};
+
+if (typeof window === 'object') {
+  window.FirebasePaginator = FirebasePaginator;
+} else if (typeof process === 'object') {
+  module.exports = FirebasePaginator;
+}
